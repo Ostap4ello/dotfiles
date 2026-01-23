b0a46c159508a1ceafe31beec0d5e761abea461c
Ostap4ello <okpeleh@gmail.com>
2026-01-23 13:01:01 +0100

    feat: nvim: CopilotChat

--- .config/nvim/lua/plugins/lsp/copilot.lua
+++ .config/nvim/lua/plugins/lsp/copilot.lua
@@ -8,4 +8,23 @@ return {
     {
         "github/copilot.vim",
     },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        build = "make tiktoken",
+        opts = {
+            model = "gpt-4.1", -- AI model to use
+            temperature = 0.1, -- Lower = focused, higher = creative
+            window = {
+                layout = "vertical", -- 'vertical', 'horizontal', 'float'
+                width = 0.5, -- 50% of screen width
+            },
+            auto_insert_mode = false, -- Enter insert mode when opening
+        },
+        keys = {
+            { "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "CopilotChat Toggle" },
+        },
+    },
 }
