bac06ea6bdc8c4abb4e51cd3c739679727a5c973
Ostap4ello <okpeleh@gmail.com>
2026-02-22 12:28:05 +0100

    fix: nvim: conform: addeed tex formatter

--- .config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/lua/plugins/lsp/conform.lua
@@ -12,7 +12,7 @@ return {
         },
     },
     opts = {
-        notify_on_error = false,
+        notify_on_error = true,
         -- format_on_save = function(bufnr)
         -- 	-- Disable "format_on_save lsp_fallback" for languages that don't
         -- 	-- have a well standardized coding style. You can add additional
@@ -34,6 +34,7 @@ return {
             assembly = { "asmfmt" },
             c = { "clang-format_custom" },
             cpp = { "clang-format_custom" },
+            tex = { "tex-fmt" },
         },
         formatters = {
             stylua_custom = {
