6c4dd113758d59125e9c55eebe9c2b559b782d72
Ostap4ello <okpeleh@gmail.com>
2026-05-14 15:38:03 +0200

    fear(nvim): color-picker command with help for Ccc

--- .config/nvim/lua/plugins/misc/colors.lua
+++ .config/nvim/lua/plugins/misc/colors.lua
@@ -13,6 +13,17 @@ return {
                     lsp = true,
                 },
             })
+
+            vim.api.nvim_create_user_command("ColorPicker", function()
+                vim.print(
+                    "Color picker binds:\n"
+                        .. " i - change input format\n"
+                        .. " o - change output format\n"
+                        .. " h/l - change value of the row\n"
+                        .. " H/M/L (0/50/100%), 1..9 (10..90%) - set value of the slider\n"
+                        .. " q/<Enter> - cancel/save\n"
+                )
+            end, { range = true, desc = "CccPick + help" })
         end,
     },
 }
